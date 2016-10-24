1ed7179ebc4e7b1f9bfe2ddca4e94aa6c1460f46
wait <992897146@qq.com>
2016-10-24 20:06:56 +0800

    HTTP请求

---
+++ app/src/main/java/com/tac/iparttimejob/NetWork/Connect/HttpAddress.java
@@ -0,0 +1,13 @@
+package com.tac.iparttimejob.NetWork.Connect;
+
+import com.tac.iparttimejob.NetWork.SignUp.SignUp;
+
+/**
+ * Created by wait on 2016/10/24.
+ */
+
+public class HttpAddress {
+    public static final String HOST                    ="";
+    public static final String SIGNUP_ADDRESS          ="";
+    public static final String LOGIN_ADDRESSS          ="";
+}
