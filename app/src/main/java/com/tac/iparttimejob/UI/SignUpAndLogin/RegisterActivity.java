5e9c5b4700b2220ef843fd9fb5496046432712fc
aiprogram <1046929908@qq.com>
2016-10-25 19:48:44 +0800

    进一步美化了注册界面

---
+++ app/src/main/java/com/tac/iparttimejob/UI/SignUpAndLogin/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.tac.iparttimejob.UI.SignUpAndLogin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tac.iparttimejob.R;
+
+/**
+ * Created by AiProgram on 2016/10/25.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
