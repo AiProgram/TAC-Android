b8dfe77867983a4b469a7c34c71a53c6d4936e83
wait <992897146@qq.com>
2016-11-15 19:42:00 +0800

    11.14

--- app/src/main/java/com/tac/iparttimejob/UI/Admin/UncheckingJobList.java
+++ app/src/main/java/com/tac/iparttimejob/UI/Admin/UncheckingJobList.java
@@ -154,7 +154,7 @@ public class UncheckingJobList extends Fragment{
         page=1;
         getList.put("page",(page)+"");
         getList.put("rows",(rows)+"");
-
+        getList.put("rstatus","0");
         //分未报名和已报名列表
             QueryInformation.getRecruitListForManager(getList, new HttpCallBackListener() {
                 @Override
@@ -188,7 +188,7 @@ public class UncheckingJobList extends Fragment{
         Map<String,String> getList=new LinkedHashMap<>();
         getList.put("page",(page)+"");
         getList.put("rows",(rows)+"");
-
+        getList.put("rstatus","0");
 
         //获得新的上拉更多的数据前禁止上拉更多
         rv_unchecking_job_list.setLoadMoreEnable(false);
